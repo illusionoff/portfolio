f8eb7185a7794db3b5159c803b104e15f385fea3
illusionoff <venom12@inbox.ru>
2021-06-09 20:03:27 +0300

    background foto and text #Intro css

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -4,8 +4,9 @@ import React from 'react';
 
 export const Intro = () => {
   return (
-    <div>
-      <h3 className="page-title white-text">intro</h3>
+    <>
+      {/* <h3 className="page-title white-text bg-image">intro</h3> */}
+      <div class="overlay"></div>
       <div className="container">
 
         <h5>Junior Node.js Back-end, Full Stack Node.js Developer</h5>
@@ -14,6 +15,6 @@ export const Intro = () => {
                      и возможности приносить пользу</p>
 
       </div>
-    </div>
+    </>
   )
 }
