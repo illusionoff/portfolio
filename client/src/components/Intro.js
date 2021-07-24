312b20f0020170cfecc95d0a91fc421ef6b0c967
illusionoff <venom12@inbox.ru>
2021-07-24 15:17:49 +0300

    Intro phrase

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -6,13 +6,12 @@ export const Intro = () => {
   return (
     <>
       {/* <h3 className="page-title white-text bg-image">intro</h3> */}
-      <div class="overlay"></div>
+      <div className="overlay"></div>
       <div className="container">
 
         <h5 ><span className="strong nowrap">Junior Node.js Back-end,</span> Full Stack Node.js Developer</h5>
 
-        <p>Я получаю удовольствие и страсть от возможности создания
-                     и возможности приносить пользу</p>
+        <h6>Занимаясь тем, что любишь - принесешь пользу себе и людям ,и непременно придешь к успеху</h6>
 
       </div>
     </>
