5f141903ab0b40a89ee0a9f0ec8e5e27aaea4607
illusionoff <venom12@inbox.ru>
2021-09-20 19:39:02 +0300

    'Intro'  React

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -9,7 +9,7 @@ export const Intro = () => {
       <div className="overlay"></div>
       <div className="container">
 
-        <h5 ><span className="strong nowrap">Junior Node.js Back-end,</span> Full Stack Node.js Developer</h5>
+        <h5 ><span className="strong nowrap">Junior Node.js Back-end,</span> Full Stack Node.js React Developer</h5>
 
         <h6>Занимаясь тем, что любишь - принесешь пользу себе и людям ,и непременно придешь к успеху</h6>
 
