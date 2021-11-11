93d39bdf2317115b127046b45abca15f98b2234c
MiniPC <venom12@inbox.ru>
2021-11-11 10:30:14 +0300

    css readability navbar logo font-size, text-newline link homemoneycalcnew

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -5,14 +5,10 @@ import React from 'react';
 export const Intro = () => {
   return (
     <>
-      {/* <h3 className="page-title white-text bg-image">intro</h3> */}
       <div className="overlay"></div>
       <div className="container">
-
         <h5 ><span className="strong nowrap">Junior Node.js Back-end,</span> Full Stack Node.js React Developer</h5>
-
-        <h6>Занимаясь тем, что любишь - принесешь пользу себе и людям ,и непременно придешь к успеху</h6>
-
+        <h6>Занимаясь тем, что любишь - принесешь пользу себе и людям, и непременно придешь к успеху</h6>
       </div>
     </>
   )
